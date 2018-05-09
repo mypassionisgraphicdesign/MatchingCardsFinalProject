fe9b6e0f72e841459381b90e89505ada0d61b11b
mypassionisgraphicdesign <doyoung.ryu@student.nido.cl>
2018-05-09 15:03:10 -0300

    v2

--- app/src/main/java/com/example/student/cardsmatchinggame/MainActivity.java
+++ app/src/main/java/com/example/student/cardsmatchinggame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.student.cardsmatchinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    int[] cardsArray = {R.drawable.ace, R.drawable.two, R.drawable.three};
+
+    public void begintorandomize(View view) {
+        Log.i("Random", "Randomy");
+    }
+
+    }
