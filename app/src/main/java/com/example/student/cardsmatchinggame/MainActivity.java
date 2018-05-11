6bb207e96d1766e3ac1f370857ce8cd81ad237c5
JacquesAV0 <jacques.venter@student.nido.cl>
2018-05-11 11:08:33 -0300

    I have commitment issues 2.0

--- app/src/main/java/com/example/student/cardsmatchinggame/MainActivity.java
+++ app/src/main/java/com/example/student/cardsmatchinggame/MainActivity.java
@@ -5,22 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+
+    int[] cardsArray = {R.drawable.ace, R.drawable.two, R.drawable.three};
+
+    public void begintorandomize(View view)
+    {
+        Log.i("Random", "Randomy");
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    int[] cardsArray = {R.drawable.ace, R.drawable.two, R.drawable.three};
-
-    public void begintorandomize(View view) {
-        Log.i("Random", "Randomy");
-    }
 
 //https://social.msdn.microsoft.com/Forums/vstudio/en-US/77ed73c3-fb56-414a-bacf-0cdcd41afff4/remove-element-from-an-array-of-string?forum=csharpgeneral
 //https://stackoverflow.com/questions/13739785/removing-an-element-from-string-array-in-android
 //https://www.geeksforgeeks.org/remove-element-arraylist-java/
 
-    }
+
+}
